1d08f410f63f38a9f72a4125125a8953539223d7
Karev Vitaliy <karev@energozapas.ru>
2020-12-30 08:09:51 +0700

    fix select static methods

--- src/main/groovy/com/github/wwkarev/gorm/Select.groovy
+++ src/main/groovy/com/github/wwkarev/gorm/Select.groovy
@@ -5,39 +5,39 @@ package com.github.wwkarev.gorm
  * @author Vitalii Karev (wwkarev)
  */
 class Select {
-    Model get(sql, Class modelClass, String fieldName, value) {
+    static Model get(sql, Class modelClass, String fieldName, value) {
         return new Selector(sql, modelClass).get(fieldName, value)
     }
 
-    Model get(sql, Class modelClass, Q q) {
+    static Model get(sql, Class modelClass, Q q) {
         return new Selector(sql, modelClass).get(q)
     }
 
-    List<Model> filter(sql, Class modelClass, String fieldName, value) {
+    static List<Model> filter(sql, Class modelClass, String fieldName, value) {
         return new Selector(sql, modelClass).filter(fieldName, value)
     }
 
-    List<Model> filter(sql, Class modelClass, Q q) {
+    static List<Model> filter(sql, Class modelClass, Q q) {
         return new Selector(sql, modelClass).filter(q)
     }
 
-    Selector where(sql, Class modelClass, String fieldName, value) {
+    static Selector where(sql, Class modelClass, String fieldName, value) {
         return new Selector(sql, modelClass).where(fieldName, value)
     }
 
-    Selector where(sql, Class modelClass, Q q) {
+    static Selector where(sql, Class modelClass, Q q) {
         return new Selector(sql, modelClass).where(q)
     }
 
-    Selector orderBy(sql, Class modelClass, List<String> fieldNames) {
+    static Selector orderBy(sql, Class modelClass, List<String> fieldNames) {
         return new Selector(sql, modelClass).orderBy(fieldNames)
     }
 
-    Selector orderBy(sql, Class modelClass, String fieldName) {
+    static Selector orderBy(sql, Class modelClass, String fieldName) {
         return new Selector(sql, modelClass).orderBy(fieldName)
     }
 
-    List<Model> select(sql, Class modelClass) {
+    static List<Model> select(sql, Class modelClass) {
         return new Selector(sql, modelClass).select()
     }
 }
