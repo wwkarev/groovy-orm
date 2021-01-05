1fa34940baf0a5af5bd1cfb238e9f956f4ebe06e
Karev Vitaliy <karev@energozapas.ru>
2021-01-05 20:32:31 +0700

    added truncate

--- src/main/groovy/com/github/wwkarev/gorm/Table.groovy
+++ src/main/groovy/com/github/wwkarev/gorm/Table.groovy
@@ -10,7 +10,12 @@ final class Table {
     static void create(Sql sql, Class modelClass) {
         new TableCreator(sql, modelClass).create()
     }
+
     static void drop(Sql sql, Class modelClass) {
         new TableDropper(sql, modelClass).drop()
+    }
+
+    static void truncate(Sql sql, Class modelClass, Boolean cascade = true) {
+        new TableTruncater(sql, modelClass).truncate(cascade)
     }
 }
